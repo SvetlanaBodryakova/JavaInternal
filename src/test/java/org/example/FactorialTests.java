762099d7be46392fb617d0680fa490080a26866b
SvetlanaBodryakova <s.badrakova@astondevs.ru>
2024-10-01 18:03:11 +0300

    add task

--- src/test/java/org/example/FactorialTests.java
+++ src/test/java/org/example/FactorialTests.java
@@ -8,22 +8,21 @@ import static org.testng.Assert.assertEquals;
 
 public class FactorialTests {
 
-    @Test(dataProvider = "provideData")
-    public void testFactorial(int number, long expResult) {
+    @Test(dataProvider = "{0}")
+    public void testFactorial(String caseName, int number, long expResult) {
         long result = Factorial.countFactorial(number);
         assertEquals(result, expResult);
     }
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
-    @Description("Факториал отрицательного числа")
+    @Test(expectedExceptions = IllegalArgumentException.class, description = "Факториал отрицательного числа")
     public void testFactorialOfNegativeNumber() {
         Factorial.countFactorial(-2);
     }
 
-    @DataProvider
+    @DataProvider(name = "{0}")
     public Object[][] provideData() {
         return new Object[][] {
-                {7,5040},
-                {0,1,}};
+                {"Факториал положительного числа", 7,5040},
+                {"Факториал нуля", 0,1}};
     }
 }
