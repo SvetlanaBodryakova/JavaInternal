ff6bb51f04189fbd5f710985d2cf400d78af7499
SvetlanaBodryakova <s.badrakova@astondevs.ru>
2024-11-19 19:53:16 +0300

    add task

--- src/test/java/org/example/CalculatorTests.java
+++ src/test/java/org/example/CalculatorTests.java
@@ -1,36 +1,53 @@
 package org.example;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 import org.example.base.BaseTest;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Epic("Калькулятор Google")
+@Feature("Арифметические действия")
+@Owner("Svetlana Bodryakova")
 public class CalculatorTests extends BaseTest {
 
     @Test
-    @DisplayName("Проверка функциональности сложения чисел")
+    @Story("Сложение")
+    @Description("Проверка функциональности сложения чисел")
+    @Severity(SeverityLevel.CRITICAL)
     public void testAddition() {
         double result = calculator.addition();
         assertEquals(12, result);
     }
 
     @Test
-    @DisplayName("Проверка функциональности вычитания чисел")
+    @Story("Вычитание")
+    @Description("Проверка функциональности вычитания чисел")
+    @Severity(SeverityLevel.CRITICAL)
     public void testSubtraction() {
         double result = calculator.subtraction();
         assertEquals(6, result);
     }
 
     @Test
-    @DisplayName("Проверка функциональности умножения чисел")
+    @Story("Умножение")
+    @Description("Проверка функциональности умножения чисел")
+    @Severity(SeverityLevel.CRITICAL)
     public void testMultiplication() {
         double result = calculator.multiplication();
         assertEquals(27, result);
     }
 
     @Test
-    @DisplayName("Проверка функциональности деления чисел")
+    @Story("Деление")
+    @Description("Проверка функциональности деления чисел")
+    @Severity(SeverityLevel.CRITICAL)
     public void testDivision() {
         double result = calculator.division();
         assertEquals(3, result);
--- src/test/java/org/example/base/BaseTest.java
+++ src/test/java/org/example/base/BaseTest.java
@@ -1,10 +1,13 @@
 package org.example.base;
 
+import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.android.AndroidDriver;
 import org.example.App.Calculator;
 import org.example.utils.Driver;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+
+import static com.github.automatedowl.tools.AllureEnvironmentWriter.allureEnvironmentWriter;
 
 public class BaseTest {
 
@@ -14,11 +17,20 @@ public class BaseTest {
     @BeforeAll
     public static void setUp() {
         driver = Driver.getDriver();
+        setAllureEnvironment();
     }
 
     @AfterAll
     public static void tearDown() {
         if (driver != null)
             driver.quit();
+    }
+
+    static void setAllureEnvironment() {
+        allureEnvironmentWriter(
+                ImmutableMap.<String, String>builder()
+                        .put("Platform.Name", "Android")
+                        .put("Platform.Version", "11.0")
+                        .build());
     }
 }
